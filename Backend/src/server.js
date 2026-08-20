bdd8c6bb8d30d8e38687e38b2e36dfcb436c0ad4
rampathare0718 <rspathare_b24@ce.vjti.ac.in>
2026-08-20 23:02:21 +0530

    server.js fixed

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,16 +5,9 @@ const path = require("path");
 
 dotenv.config();
 
+const app = express();
+
 const connectDB = require("./config/db");
-
-app.use(cors({
-    origin: "https://cleansnap-frontend-k69j.onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
-
-app.options("*", cors());
 
 // Routes
 const authRoutes = require("./routes/authRoutes");
@@ -23,37 +16,46 @@ const userRoutes = require("./routes/userRoutes");
 const complaintRoutes = require("./routes/complaintRoutes");
 const governmentUpdateRoutes = require("./routes/governmentUpdateRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
-const rewardRoutes=require("./routes/rewardRoutes");
-const feedbackRoutes=require("./routes/feedbackRoutes");
+const rewardRoutes = require("./routes/rewardRoutes");
+const feedbackRoutes = require("./routes/feedbackRoutes");
 
-
-// Load Environment Variables
-
-
-// Connect Database
-connectDB();
-
-const app = express();
+// =========================
+// CORS
+// =========================
+app.use(cors({
+    origin: "https://cleansnap-frontend-k69j.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 // =========================
 // Middlewares
 // =========================
-
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
+// =========================
+// Connect Database
+// =========================
+connectDB();
 
+// =========================
+// Static Files
+// =========================
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// =========================
+// Routes
+// =========================
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/complaints", complaintRoutes);
 app.use("/api/government-updates", governmentUpdateRoutes);
-app.use("/api/notifications", notificationRoutes);   
-app.use("/api/rewards",rewardRoutes);  
-app.use("/api/feedback",feedbackRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/rewards", rewardRoutes);
+app.use("/api/feedback", feedbackRoutes);
 
 // =========================
 // Home Route
